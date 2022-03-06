fe193b059db25c85d899899fdf0bc6facd5528d4
Jonas Alves <jonasalves@alu.ufc.br>
2022-03-06 12:01:37 -0300

    Added dependencies

--- setup.py
+++ setup.py
@@ -8,12 +8,15 @@ with open(os.path.join(here, "README.md"), "r") as f:
 
 setup(
     name="youtube_community_tab",
-    version="0.2.2",
+    version="0.2.2.1",
     description="A python3 module to handle YouTube Community Tab",
     long_description_content_type="text/markdown",
     long_description=long_description,
     url="https://github.com/bot-jonas/youtube-community-tab",
     package_dir={"": "src"},
+    install_requires=[
+        "requests_cache",
+    ],
     packages=find_packages(where="src"),
     zip_safe=False,
 )
